630390ed03749ab96052c3889bc7cead18318cd4
pathu <pathumdeeptha9@gmail.com>
2021-11-24 17:02:33 +0530

    Added grade to the exisiting Main.java file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,17 @@ public class Main {
         int mk3 = 99;
         int total = mk1 + mk2 + mk3;
         int avg = total / 3;
+        String grade;
+
+        if (avg >= 50){
+            grade = "Pass";
+        }else {
+            grade = "Fail";
+        }
 
         System.out.println("Total : "+total);
         System.out.println("Average : "+avg);
+        System.out.println("Grade : "+grade);
 
     }
 }
