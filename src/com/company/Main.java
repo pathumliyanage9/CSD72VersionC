ce1efb29fceec0ef34e746832f7ae5b4a98821bf
pathu <pathumdeeptha9@gmail.com>
2021-11-24 16:58:59 +0530

    Designed a basic calculation program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi, this from main method!");
+        int mk1 = 67;
+        int mk2 = 99;
+        int mk3 = 99;
+        int total = mk1 + mk2 + mk3;
+        int avg = total / 3;
+
+        System.out.println("Total : "+total);
+        System.out.println("Average : "+avg);
+
     }
 }
